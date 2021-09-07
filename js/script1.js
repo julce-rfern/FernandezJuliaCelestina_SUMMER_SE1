1250fdbd9f17d32a8ebce658c8d20c8b6fb4cc5a
Jace Fernandez <juliacelestina.fernandez.iics@ust.edu.ph>
2021-09-08 01:07:13 +0800

    added script1.js

---
+++ js/script1.js
@@ -0,0 +1,13 @@
+class myPet
+{
+    constructor(type,name,breed,age,gender)
+    {
+        this.type=type
+        this.name=name
+        this.breed=breed
+        this.age=age
+        this.gender=gender
+        this.talk=function(){}
+        this.walk=function(){}
+    }
+}
